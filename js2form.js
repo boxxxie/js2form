7e5e7a790dcc1ff642e4d4445cb880281468ffef
Paul <somethingitalian@gmail.com>
2011-10-17 02:54:21 -0400

    removed object2array function and all dependancies. added comments to each function added a pather function which will allow users to use their own variable name notation. fixed a bug in the default path translator (it now should work on arrays and nested arrays)

--- js2form.js
+++ js2form.js
@@ -3,124 +3,82 @@
  * underscore.js
  * jQuery.js
  */
-var populateForm = (function (){
-     function isSimpleVal(val){
-	 if(_.isArray(val)){return false;}
-	 if(_.isString(val)||
-	    _.isNumber(val)||
-	    _.isBoolean(val)||
-	    _.isDate(val)||
-	    _.isNaN(val)||
-	    _.isNull(val)||
-	    _.isUndefined(val)){return true;}
-	 return false;
-     }
-     function valueIsSimple(item){
-	 return isSimpleVal(item.value);
-     }
-     function valueIsComplex(item){
-	 return !valueIsSimple(item);
-     }
-     function applyPrefix(name,prefix){
-	 if(_.isUndefined(prefix)){
-	     return name;
-	 }
-	 else if(_.isNumber(parseInt(name))){
-	     return prefix + "["+ name +"]";
-	 }
-	 else{
-	     return prefix + "." + name;
-	 }
-     };
-     function object2array(obj,prefix){
-	 var keys = _.keys(obj);
-	 var vals = _.values(obj);
-	 var firstStep = _(_.zip(keys,vals)).chain()
-	     .map(function(keyVal){
-		      var key = keyVal[0];
-		      var val = keyVal[1];
-		      return {name: applyPrefix(key,prefix), value:val};
-		  })
-	     .value();
-	 var completed = _.filter(firstStep,valueIsSimple);
-	 var toBeWorkedOn = _.filter(firstStep,valueIsComplex);
-	 if(_.isEmpty(toBeWorkedOn)){
-	     return completed;
-	 }
-	 else{
-	     return _(toBeWorkedOn)
-		 .chain()
-		 .map(function(item)
-		      {return object2array(item.value,item.name);})
-		 .flatten()
-		 .union(completed)
-		 .value();
-	 }
-     };
-     function recursiveTrav(obj,travel){
-	 if(_.isEmpty(travel)){
-	     return obj;
-	 }
-	 if(!obj){
-	     return null;
-	 }
-	 var prop = _.first(travel);
-	 return recursiveTrav(obj[prop],_.rest(travel));
-     };
-     function recursiveAssign(obj,travel,toAssign){
-	 if(_.isEmpty(travel)){
-	     obj = toAssign;
-	     return obj;
-	 }
-	 if(!obj){
-	     return null;
-	 }
-	 var prop = _.first(travel);
-	 obj[prop] = recursiveAssign(obj[prop],_.rest(travel),toAssign);
-	 return obj;
-     };
-     //matches a name from {name: ...} to a string
-     function nameMatch(name){
-	 return function(item){
-	     return (item.name == name);
+var populateForm = 
+    (function (){
+	 function getFromPath(obj,travel){
+	     //walks a path defined by an array of fields, returns the last field walked
+	     if(_.isEmpty(travel)){
+		 return obj;
+	     }
+	     if(!obj){
+		 return null;
+	     }
+	     var prop = _.first(travel);
+	     return getFromPath(obj[prop],_.rest(travel));
 	 };
-     };
-     function nodesProcessor(obj,nodes,callback){
-	 _.each(nodes,function(node){ 
-		    var $node = $(node);
-		    var varType = $node.attr('var_type');
-		    var nodeName = $node.attr('name');
-		    var varPath = nodeName.split(".");
-		    var objPropToChange = recursiveTrav(obj,varPath);
-
-		    if(nodeName || objPropToChange){
-			obj = recursiveAssign(obj,
-					      varPath,
-					      callback(objPropToChange,$node));
-		    }
-		});
-	 return obj;
-     };
-     function formElementPopulator($node,value){
-	 $node.val(value);
-	 if($node.attr('type') == 'checkbox' && value){
-	     $node.attr('checked',value);
-	 }
-     };
-     return function populateForm(js,transformer){
-	 var nodesToPopulate = document.querySelectorAll('[name]');
-	 if(!_.isUndefined(transformer)){
-	     js = nodesProcessor(js,nodesToPopulate,transformer);
-	 }
-	 var arr = object2array(js);
-	 _(nodesToPopulate)
-	     .chain()
-	     .each(function(item){
-		       var $item = $(item);
-		       var nameAtt = $item.attr('name');
-		       var itemForForm = _.detect(arr,nameMatch(nameAtt));
-		       var valForForm = itemForForm.value;
-		       formElementPopulator($item,valForForm);
-		   });
-     };
-})();
+	 function assignFromPath(obj,travel,assignVal){
+	     //walks a path defined by an array of fields, assigns a value to the last field walked
+	     if(_.isEmpty(travel)){
+		 obj = assignVal;
+		 return obj;
+	     }
+	     if(!obj){
+		 return null;
+	     }
+	     var prop = _.first(travel);
+	     obj[prop] = assignFromPath(obj[prop],_.rest(travel),assignVal);
+	     return obj;
+	 };	 
+	 function jsPather(pathStr){
+	     //converts js obj notation into a path array
+	    return pathStr
+		 .replace(/\[/g,'.')
+		 .replace(/\]/g,'')
+		 .split(".");
+	 };
+	 function nodesProcessor(obj,nodes,callback,pather){
+	     /*changes the shape of the obj to suit the form
+	      *e.g. {i : ["1","2","3"]} -> {i:"123"} 
+	      */
+	     if(_.isUndefined(pather)){
+		 pather = jsPather;
+	     }
+	     _.each(nodes,function(node){ 
+			var $node = $(node);
+			var varType = $node.attr('var_type');
+			var nodeName = $node.attr('name');
+			var varPath = pather(nodeName);
+			var objPropToChange = getFromPath(obj,varPath);
+			if(nodeName || objPropToChange){
+			    obj = assignFromPath(obj,
+						  varPath,
+						  callback(objPropToChange,$node));
+			}
+		    });
+	     return obj;
+	 };
+	 function formElementPopulator($node,value){
+	     //sets the value of an element in a form. some elements require special treatment (checkboxs)
+	     $node.val(value);
+	     if($node.attr('type') == 'checkbox' && value){
+		 $node.attr('checked',value);
+	     }
+	 };
+	 //FIXME: make it so that this will work on a single form. right now it operates on the whole page
+	 return function populateForm(obj,transformer,pather){
+	     var nodesToPopulate = document.querySelectorAll('[name]');
+	     if(_.isUndefined(pather)){
+		 pather = jsPather;
+	     }
+	     if(!_.isUndefined(transformer)){
+		 obj = nodesProcessor(obj,nodesToPopulate,transformer,pather);
+	     }
+	     _.each(nodesToPopulate,function(item){
+			var $item = $(item);
+			var nameAtt = $item.attr('name');
+			var path = pather(nameAtt);
+			var valForForm = getFromPath(obj,path);
+			formElementPopulator($item,valForForm);
+		    });
+	 };
+     })();
